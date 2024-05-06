88dc66cf9661a4a97ef264433f9e7c74478b02f9
JUKI <jyukipann@yahoo.co.jp>
2024-05-06 14:41:50 +0900

    データセットについて

--- src/model.py
+++ src/model.py
@@ -1,32 +1,31 @@
-import torch
 import torch.nn as nn
 
 class Generator(nn.Module):
-  def __init__(self, z_dim, img_dim):
-    super().__init__()
-    self.fc1 = nn.Linear(z_dim, 256)
-    self.fc2 = nn.Linear(256, 512)
-    self.fc3 = nn.Linear(512, img_dim)
+    def __init__(self, z_dim, img_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(z_dim, 256)
+        self.fc2 = nn.Linear(256, 512)
+        self.fc3 = nn.Linear(512, img_dim)
 
-  def forward(self, z):
-    out = self.fc1(z)
-    out = nn.ReLU()(out)
-    out = self.fc2(out)
-    out = nn.ReLU()(out)
-    out = self.fc3(out)
-    return out
+    def forward(self, z):
+        out = self.fc1(z)
+        out = nn.ReLU()(out)
+        out = self.fc2(out)
+        out = nn.ReLU()(out)
+        out = self.fc3(out)
+        return out
 
 class Discriminator(nn.Module):
-  def __init__(self, img_dim):
-    super().__init__()
-    self.fc1 = nn.Linear(img_dim, 512)
-    self.fc2 = nn.Linear(512, 256)
-    self.fc3 = nn.Linear(256, 1)
+    def __init__(self, img_dim):
+        super().__init__()
+        self.fc1 = nn.Linear(img_dim, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, 1)
 
-  def forward(self, img):
-    out = self.fc1(img)
-    out = nn.ReLU()(out)
-    out = self.fc2(out)
-    out = nn.ReLU()(out)
-    out = self.fc3(out)
-    return out
+    def forward(self, img):
+        out = self.fc1(img)
+        out = nn.ReLU()(out)
+        out = self.fc2(out)
+        out = nn.ReLU()(out)
+        out = self.fc3(out)
+        return out
---
+++ src/playground.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+import utils
+
+if __name__ == '__main__':
+    print('playground')
+    
+    # name of the file
+    fmaps = 'Maps_T_SIMBA_LH_z=0.00.npy'
+    maps = np.load(fmaps)
+    
+    
