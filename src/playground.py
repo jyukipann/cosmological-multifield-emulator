7e989cadd371b2a583304cde38f1a95780410338
JUKI <jyukipann@yahoo.co.jp>
2024-05-07 16:15:53 +0900

    初めてのプロットとマラーマップの表示成功

--- src/playground.py
+++ src/playground.py
@@ -1,12 +1,34 @@
-import cv2
 import numpy as np
 import utils
+import pathlib
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     print('playground')
+    out_dir = pathlib.Path("dump/")
+    out_dir.mkdir(parents=True, exist_ok=True)    
     
-    # name of the file
-    fmaps = 'Maps_T_SIMBA_LH_z=0.00.npy'
-    maps = np.load(fmaps)
+    def load_map0_and_plot(fmaps, cmap=plt.cm.hot, log=True):
+        maps = np.load(fmaps)
+        print(maps.shape)
+        plt.clf()
+        if log:
+            maps[0] = np.log(maps[0])
+        plt.pcolor(maps[0], cmap=cmap)
+        plt.colorbar()    
+    
+    fmaps = 'dataset/Maps_Mgas_IllustrisTNG_CV_z=0.00.npy'
+    load_map0_and_plot(fmaps=fmaps, cmap=plt.cm.hot)
+    plt.savefig(out_dir/"first_plot_Mgas_hot.png")
+    
+    fmaps = 'dataset/Maps_HI_IllustrisTNG_CV_z=0.00.npy'
+    load_map0_and_plot(fmaps=fmaps, cmap=plt.cm.Greens)
+    plt.savefig(out_dir/"first_plot_HI.png")
+    
+    
+    fmaps = 'dataset/Maps_B_IllustrisTNG_CV_z=0.00.npy'
+    load_map0_and_plot(fmaps=fmaps, cmap=plt.cm.cividis)
+    plt.savefig(out_dir/"first_plot_B.png")
+    
     
     
