61093afac5d012f559034db8cdaf19f8e4c0fd4d
manaTerazawa <133829269+manaTerazawa@users.noreply.github.com>
2024-07-17 18:03:29 +0900

    calculate_gradient_penalty関数の実装

--- src/train.py
+++ src/train.py
@@ -97,13 +97,11 @@ def train(
 
         # Wasserstein loss
         # 上記のloss計算とこのloss計算はどっちかだけ使う
-        gradient_penalty = 0  # 仮
-        # gradient_penalty = calculate_gradient_penalty(discriminator, batch.data, fake_inputs.data, device)
+        # gradient_penalty = 0  # 仮
+        gradient_penalty = calculate_gradient_penalty(discriminator, batch, batch_low_res, fake_inputs, low_res_fake_inputs, device)
+        # gradient_penalty = calculate_gradient_penalty(discriminator, batch, fake_inputs, device)
         lambda_gp = 10          # 仮
 
-        # print(f"len(batch): {len(batch)}, len(batch.data): {len(batch.data)}, batch.size(0): {batch.size(0)}")
-        # print(f"len(fake_inputs): {len(fake_inputs)}, len(fake_inputs.data): {len(fake_inputs.data)}")
-        
         lossD_r = torch.mean(torch.minimum(torch.zeros_like(real_outputs), -1 + real_outputs))  # real loss
         lossD_f = torch.mean(torch.minimum(torch.zeros_like(fake_outputs), -1 - fake_outputs))  # fake loss
         loss_discriminator = -lossD_r - lossD_f + rec_loss
@@ -143,6 +141,8 @@ def train(
             #     "train/Loss_D_focal", focal_loss, global_step)
             summary_writer.add_scalar(
                 "train/Loss_D", loss_discriminator, global_step)
+            summary_writer.add_scalar(
+                "train/gradient_penalty", gradient_penalty, global_step)
     print(f"{epoch=} : loss_generator={loss_sum_G/max_step}, loss_discriminator={loss_sum_D/max_step}")
 
 def val(
@@ -215,40 +215,21 @@ def noise_schedule(epoch, coffitent, half_epoch, cutoff_epoch=None)->float:
         return 1/(1+torch.e**(coffitent*(epoch-half_epoch)))
     return 0
 
-def calculate_gradient_penalty(D, real_img, fake_img, device):
+def calculate_gradient_penalty(D, real_input, real_low_res, fake_input, fake_low_res, device):
     """Calculates the gradient penalty loss for WGAN GP"""
     # Random weight term for interpolation between real and fake samples
-    # alpha = torch.Tensor(np.random.random((real_img.size(0), 1, 1, 1))).to(device)
-    # # Get random interpolation between real and fake samples
-    # interpolates = (alpha * real_img + ((1 - alpha) * fake_img)).requires_grad_(True)
-    # d_interpolates, _, _, _ = D(interpolates)
-    # fake = torch.autograd.Variable(torch.Tensor(real_img.shape[0], 1).fill_(1.0), requires_grad=False)
-    # # Get gradient w.r.t. interpolates
-    # gradients = torch.autograd.grad(
-    #     outputs=d_interpolates,
-    #     inputs=interpolates,
-    #     grad_outputs=fake,
-    #     create_graph=True,
-    #     retain_graph=True,
-    #     only_inputs=True,
-    # )[0]
-    # gradients = gradients.view(gradients.size(0), -1)
-    # gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
-    # return gradient_penalty
-
-    alpha  = torch.rand(real_img.size(0), 1, 1, 1).to(device)  # (100, 1, 1, 1)
+    alpha  = torch.rand(real_input.size(0), 1, 1, 1).to(device)  # (100, 1, 1, 1)
     
     # Get random interpolation between real and fake samples
-    interpolates = alpha * real_img + (1-alpha) * fake_img
-    interpolates.requires_grad = True
-
-    print(real_img.shape, fake_img.shape, interpolates.shape)
+    interpolates = alpha * real_input + (1-alpha) * fake_input
+    interpolates_low_res = alpha * real_low_res + (1-alpha) * fake_low_res
+    # interpolates.requires_grad = True
+    # interpolates_low_res.requires_grad = True
 
     # Discriminator output for interpolated samples
-    d_interpolates = D(interpolates)
-
+    d_interpolates, _, _, _ = D(interpolates, interpolates_low_res)
     # Fake labels for interpolated samples (all ones)
-    fake = torch.ones((real_img.size(0), 1), device = device)
+    fake = torch.ones((real_input.size(0), 1), device = device)
 
     # Calculate gradients w.r.t. interpolated samples
     gradients = torch.autograd.grad(
@@ -259,6 +240,8 @@ def calculate_gradient_penalty(D, real_img, fake_img, device):
     
     gradients = gradients.view(gradients.size(0), -1)
     gradient_penalty = ((gradients.norm(2, dim=1) - 1) ** 2).mean()
+
+    # print(f"gradient penalty: {gradient_penalty}")
     
     return gradient_penalty
 
@@ -285,7 +268,7 @@ def train_loop():
 
     # train configs
     batch_size = 100
-    max_epoch = 300
+    max_epoch = 300  # default: 300
     val_inerval = 10
     checkpoint_interval = 10
     lr = 0.0001
